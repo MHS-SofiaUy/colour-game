315b35a439de83cca074835d2690340844deaa4f
MHS-SofiaUy <86634615+MHS-SofiaUy@users.noreply.github.com>
2024-05-10 11:49:55 +1200

    Add files via upload
    working help button in base code
    :3

--- 06_help_v1.py
+++ 06_help_v1.py
@@ -6,53 +6,9 @@ import random
 
 # users choose 3, 5 or 10 rounds
 class ChooseRounds:
-
     def __init__(self):
-        button_fg = "#FFFFFF"
-        button_font = ("Arial", "13", "bold")
-
-        # Set up GUI frame
-        self.intro_frame = Frame(padx=10, pady=10)
-        self.intro_frame.grid()
-
-        # heading and brief instructions
-        self.intro_heading_label = Label(self.intro_frame, text="Colour Quest",
-                                         font=("Arial", "16", "bold"))
-        self.intro_heading_label.grid(row=0)
-
-        choose_instructions_txt = "In each round you will be given" \
-                                  "six different colours to choose" \
-                                  "from. Pick a colour and see if" \
-                                  "you can beat the computer's score!\n\n" \
-                                  "To begin, choose how many rounds" \
-                                  "you'd like to play..."
-        self.choose_instructions_label = Label(self.intro_frame,
-                                               text=choose_instructions_txt,
-                                               wraplength=300,
-                                               justify="left")
-        self.choose_instructions_label.grid(row=1)
-
-        # Rounds buttons...
-        self.how_many_frame = Frame(self.intro_frame)
-        self.how_many_frame.grid(row=2)
-
-        # list to set up rounds button. First item in each
-        # sublist is the background colour, second item is
-        # the number of rounds
-        btn_colour_value = [
-            ["#CC0000", 3], ["#009900", 5], ["#000099", 10]
-        ]
-
-        for item in range(0, 3):
-            self.rounds_button = Button(self.how_many_frame,
-                                        fg=button_fg,
-                                        bg=btn_colour_value[item][0],
-                                        text="{} Rounds".format(btn_colour_value[item][1]),
-                                        font=button_font, width=10,
-                                        command=lambda i=item: self.to_play(btn_colour_value[i][1])
-                                        )
-            self.rounds_button.grid(row=0, column=item,
-                                    padx=5, pady=5)
+        # invoke play class with three rounds for testing purposes.
+        self.to_play(3)
 
     def to_play(self, num_rounds):
         Play(num_rounds)
@@ -150,7 +106,7 @@ class Play:
         self.next_button = Button(self.rounds_frame, text="Next Round",
                                   fg="#FFFFFF", bg="#008BFC",
                                   font=("Arial", 11, "bold"),
-                                  width=10, state=DISABLED, command=self.new_round)
+                                  width=10, state=DISABLED)
         self.next_button.grid(row=0, column=1)
 
         # at start, get 'new round'
@@ -190,15 +146,7 @@ class Play:
             # Add buttons to control list
             self.control_button_ref.append(self.make_control_button)
 
-        # Access stats and help buttons so that they can be
-        # enabled / disabled
-        self.to_help_button = self.control_button_ref[0]
-        self.to_stats_btn = self.control_button_ref[1]
-        self.to_start_over_btn = self.control_button_ref[2]
-
-        # disable stats button at start of game (as there
-        # are no stats to display)
-        self.to_stats_btn.config(state=DISABLED)
+            self.to_help_btn = self.control_button_ref[0]
 
     # retrieve colours from csv file
     def get_all_colours(self):
@@ -336,28 +284,28 @@ class Play:
                                                      comp_total)
             self.game_results_label.config(text=game_outcome_txt)
 
-        # if the game is over, disable all buttons
-        # and change text of 'next' button to either
-        # 'You Win' or 'You Lose' amd disable all buttons
+            # if the game is over, disable all buttons
+            # and change text of 'next' button to either
+            # 'You Win' or 'You Lose' amd disable all buttons
 
-        if current_round == how_many:
-            # change 'next' button to show overall
-            # win / loss result and disable it
-            self.next_button.config(state=DISABLED,
-                                    text=status)
+            if current_round == how_many:
+                # change 'next' button to show overall
+                # win / loss result and disable it
+                self.next_button.config(state=DISABLED,
+                                        text=status)
 
-            # update 'start over button'
-            start_over_button = self.control_button_ref[2]
-            start_over_button['text'] = "Play Again"
-            start_over_button['bg'] = "#009900"
+                # update 'start over button'
+                start_over_button = self.control_button_ref[2]
+                start_over_button['text'] = "Play Again"
+                start_over_button['bg'] = "#009900"
 
-            # change all colour button background to light grey
-            for item in self.choice_button_ref:
-                item.config(bg='#C0C0C0')
+                # change all colour button background to light grey
+                for item in self.choice_button_ref:
+                    item.config(bg='#C0C0C0')
 
-        else:
-            # enable next round button and update heading
-            self.next_button.config(state=NORMAL)
+            else:
+                # enable next round button and update heading
+                self.next_button.config(state=NORMAL)
 
     # detects which 'control' button was pressed and
     # invokes necessary function. Can possibly replace functions
@@ -374,7 +322,7 @@ class Play:
         print("You chose to get the statistics")
 
     def get_help(self):
-        DisplayHelp(self)
+        print("You chose to get help")
 
     # DON'T USE THIS FUNCTION IN BASE AS IT KILLS THE ROO
     def close_play(self):
@@ -394,6 +342,14 @@ class DisplayHelp:
 
         # If users press cross at top, closes help and
         # 'releases' help button
+
+        self.to_help_button = Button(self.button_frame,
+                                     text="Help / Info",
+                                     bg="#CC6600",
+                                     width=12,
+                                     command=self.to_help)
+        self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
+
         self.help_box.protocol('WM_DELETE_WINDOW',
                                partial(self.close_help, partner))
 
